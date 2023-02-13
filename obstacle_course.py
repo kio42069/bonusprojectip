b976caf73d05892638aabef6c1b8fcdb4f217640
Surat Sathi Samanta <62372847+surat-ss@users.noreply.github.com>
2023-02-13 21:12:20 +0530

    initial bg testing for animatoin done

--- obstacle_course.py
+++ obstacle_course.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 1000, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -6,16 +7,29 @@ pygame.display.set_caption("HydroHopper")
 
 
 FPS = 60
+
+
+def draw_win(bg):
+    WIN.blit(bg, (0,0))
+    pygame.display.update()
     
 def main():
-    clock = pygame.event.Clock()
+    clock = pygame.time.Clock()
     run = True
+    x = 1
     while run:
+        bg = pygame.transform.scale((pygame.image.load(os.path.join('Assets_Game',f'bgs{x}.png'))),(WIDTH,HEIGHT))
+        x += 1
+        if x > 3:
+            x = 1
+            print(1)
+        draw_win(bg)
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+        
     main()
     
 if __name__ == "__main__":
