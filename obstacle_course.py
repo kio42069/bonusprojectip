095ed2ca9056ce323fd3f6d03bf7aa9615fd2c6a
Surat Sathi Samanta <62372847+surat-ss@users.noreply.github.com>
2023-02-14 17:11:36 +0530

    fufck im dumb

--- obstacle_course.py
+++ obstacle_course.py
@@ -2,22 +2,23 @@ import pygame
 import os
 import random
 pygame.font.init()
+pygame.mixer.init()
 
 SCORE_FONT = pygame.font.SysFont('georgia', 40)
 GAME_END  = pygame.font.SysFont('georgia', 100)
 HEALTH_FONT = pygame.font.SysFont('georgia', 40)
 MAN_WIDTH, MAN_HEIGHT = 100, 100
-ROCK_WIDTH, ROCK_HEIGHT = 50,50
-WIDTH, HEIGHT = 1000, 500
+ROCK_WIDTH, ROCK_HEIGHT = 100,40
+WIDTH, HEIGHT = 1000,500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("HydroHopper")
+CRASH_SOUND = pygame.mixer.Sound(os.path.join('Assets_game', 'crash.wav'))
 
 FREQUENCY = 0.4
 MAN_HIT = pygame.USEREVENT
 OBSTACLE_VEL = 10
 VEL = 5
 FPS = 60
-ROCK = pygame.transform.scale(pygame.image.load(os.path.join('Assets_Game','rock.png')),(ROCK_WIDTH, ROCK_HEIGHT))
 MAN = pygame.transform.scale(pygame.image.load(os.path.join('Assets_Game','man.png')),(MAN_WIDTH, MAN_HEIGHT))
 
 def draw_win(bg, man, obstacles, man_health, score, highscore):
@@ -29,7 +30,7 @@ def draw_win(bg, man, obstacles, man_health, score, highscore):
     WIN.blit(score_text, (WIDTH-score_text.get_width()-10,10))
     highscore_text = SCORE_FONT.render(f"Highscore: {int(highscore)}",1 ,(0,0,0))
     WIN.blit(highscore_text, (WIDTH-highscore_text.get_width()-10,10+score_text.get_height()))
-    WIN.blit(ROCK, (100,100))
+    ROCK = pygame.transform.scale(pygame.image.load(os.path.join('Assets_Game','shark.png')),(ROCK_WIDTH, ROCK_HEIGHT))
     for obstacle in obstacles:
         WIN.blit(ROCK, (obstacle.x, obstacle.y))
         
@@ -43,7 +44,7 @@ def draw_end(text):
     pygame.time.delay(1000)
     
 def movement(keys_pressed, man):
-    if keys_pressed[pygame.K_UP] and man.y > HEIGHT//2-30:
+    if keys_pressed[pygame.K_UP] and man.y > 15:
         man.y -= VEL
     if keys_pressed[pygame.K_DOWN] and man.y +VEL + man.height < HEIGHT - 15:
         man.y += VEL
@@ -62,7 +63,6 @@ def main():
     obstacles = []
     clock = pygame.time.Clock()
     run = True
-    img_num = 1
     obstacle_count = 0
     old_obstacle_count = 0
     man_health = 3
@@ -70,14 +70,12 @@ def main():
     with open("highscore.txt", "r") as file:
         recs = file.readlines()
     recs = [int(x) for x in recs]
+    recs.append(0)
     highscore = max(recs)
     while run:
-        bg = pygame.transform.scale((pygame.image.load(os.path.join('Assets_Game',f'bgs{int(img_num)}.png'))),(WIDTH,HEIGHT))
-        img_num += 0.1
+        bg = pygame.transform.scale((pygame.image.load(os.path.join('Assets_Game','ocean.jpg'))),(WIDTH,HEIGHT))
         obstacle_count += FREQUENCY/10
         score += 0.05
-        if img_num > 3:
-            img_num = 1
         draw_win(bg, man, obstacles, man_health, score, highscore)
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -87,9 +85,10 @@ def main():
             
             if event.type == MAN_HIT:
                 man_health -= 1
-        
+                CRASH_SOUND.play()
+
         if int(obstacle_count) > old_obstacle_count:
-            obstacle = pygame.Rect(WIDTH, random.randint(HEIGHT//2-30,HEIGHT), ROCK_WIDTH, ROCK_HEIGHT)
+            obstacle = pygame.Rect(WIDTH, random.randint(0,HEIGHT), ROCK_WIDTH, ROCK_HEIGHT)
             obstacles.append(obstacle)
             old_obstacle_count = obstacle_count
         game_end_text = ""    
---
+++ prashi_interface.py
@@ -0,0 +1,33 @@
+'''from tkinter import *
+from tkinter.ttk import *
+
+root=Tk()
+
+root.title('gawkgawk3000')
+root.geometry('600x600')
+label=Label(root,text='swapnil fucks surat fast and hard').pack()
+
+button=Button(root,text='rope',bd=5).pack()
+button=Button(root,text='handcuffs').pack()
+button=Button(root,text='dildo').pack()
+button=Button(root,text='whip').pack()
+
+
+root.mainloop()'''
+
+''from tkinter import *   
+ 
+# create a tkinter window
+root = Tk()             
+root.title('gawkgawk3000')
+# Open window having dimension 100x100
+root.geometry('600x600')
+ 
+# Create a Button
+btn = Button(root, text = 'Click me !', bd = '5',
+                          command = root.destroy)
+ 
+# Set the position of button on the top of window.  
+btn.pack(side = 'top')   
+ 
+root.mainloop()
