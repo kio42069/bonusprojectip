1bc2ab94fb4a82d728138f53c60aa90780c1600a
Surat Sathi Samanta <62372847+surat-ss@users.noreply.github.com>
2023-02-15 20:14:37 +0530

    i hate myself sm

--- daredive.py
+++ daredive.py
@@ -1,4 +1,5 @@
-def kek():
+def daredive():
+    import button
     import pygame
     import os
     import random
@@ -6,15 +7,20 @@ def kek():
     pygame.mixer.init()
     from time import time
 
-    SCORE_FONT = pygame.font.SysFont('georgia', 40)
-    GAME_END  = pygame.font.SysFont('georgia', 100)
-    HEALTH_FONT = pygame.font.SysFont('georgia', 40)
+    SCORE_FONT = pygame.font.SysFont('arcadeclassic', 40)
+    GAME_END  = pygame.font.SysFont('arcadeclassic', 100)
+    HEALTH_FONT = pygame.font.SysFont('arcadeclassic', 40)
     MAN_WIDTH, MAN_HEIGHT = 100, 100
     ROCK_WIDTH, ROCK_HEIGHT = 100,40
     WIDTH, HEIGHT = 1000,500
     WIN = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("HydroHopper")
     CRASH_SOUND = pygame.mixer.Sound(os.path.join('Assets_Hydrohopper', 'crash.wav'))
+    start_img = pygame.transform.scale(pygame.image.load("Start.png").convert_alpha(), (300,150))
+    exit_img = pygame.transform.scale(pygame.image.load("Quit.png").convert_alpha(), (300,150))
+            
+    start_button = button.Button(170, 180, start_img, 0.7)
+    exit_button = button.Button(620, 180, exit_img,0.7)
 
     FREQUENCY = 0.4
     MAN_HIT = pygame.USEREVENT
@@ -58,6 +64,34 @@ def kek():
             elif obstacle.x < 0:
                 obstacles.remove(obstacle)
             
+    def checker():
+        runner = True
+        while runner:
+            WIN.fill((202, 228, 241))
+            if start_button.draw(WIN):
+                main()
+                runner = False
+                break
+            if exit_button.draw(WIN):
+                pygame.quit()
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    runner = False
+    def check_test():
+        runner = True
+        while runner:
+            WIN.fill((202, 228, 241))
+            if start_button.draw(WIN):
+                main()
+                runner = False
+                break
+            if exit_button.draw(WIN):
+                pygame.quit()
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    runner = False
     def main():
         OBSTACLE_VEL = 10
         VEL = 5
@@ -67,7 +101,7 @@ def kek():
         run = True
         obstacle_count = 0
         old_obstacle_count = 0
-        man_health = 999999999
+        man_health = 3
         man = pygame.Rect(100, 300, MAN_WIDTH, MAN_HEIGHT)
         with open("highscore.txt", "r") as file:
             recs = file.readlines()
@@ -109,7 +143,6 @@ def kek():
             movement(keys_pressed, man, VEL)
         with open("highscore.txt","a") as file:
             file.write(str(int(score))+"\n")
-        main()
-    main()
-"""    if __name__ == "__main__":
-        main()"""
+    while True:
+        checker()
+        check_test()
--- prashi_interface.py
+++ prashi_interface.py
@@ -1,20 +1,4 @@
-'''from tkinter import *
-from tkinter.ttk import *
-
-root=Tk()
-
-root.title('gawkgawk3000')
-root.geometry('600x600')
-label=Label(root,text='swapnil fucks surat fast and hard').pack()
-
-button=Button(root,text='rope',bd=5).pack()
-button=Button(root,text='handcuffs').pack()
-button=Button(root,text='dildo').pack()
-button=Button(root,text='whip').pack()
-
-
-root.mainloop()'''
-'''from HYDROHOPPER import kek
+from daredive import kek
 from tkinter import *   
  
 canvas_bg = "#b2d4fa"
@@ -29,7 +13,7 @@ label=Label(root,text='swapnil fucks surat fast and hard').place(x=200,y=20)
 def submitFunction() :
     print('Submit button is clicked.')
 
-btn = Button(root, text = 'rope', bd = '5',height=5,width=30, command = kek())
+btn = Button(root, text = 'rope', bd = '5',height=5,width=30, command = lambda: kek())
 btn.place(x=200,y=40)
 btn = Button(root, text = 'whip', bd = '5',height=5,width=30)
 btn.place(x=200,y=140)  
@@ -40,36 +24,4 @@ btn.place(x=200,y=340)
 btn = Button(root, text = 'limit reached', bd = '5',height=5,width=30)
 btn.place(x=200,y=440)
 
-root.mainloop()'''
-
-from HYDROHOPPER import kek
-from tkinter import *
-'''from tkinter import ttk
-
-#Create an instance of Tkinter Frame
-win = Tk()
-
-#Set the geometry of Tkinter Frame
-win.geometry("700x350")
-
-
-#Create a Label widget
-Label(win, text= "WELCOME TO THE INTERNET !").pack(pady=15)
-
-#Create a Button for opening a dialog Box
-ttk.Button(win, text= "surat nudes", command= kek()).pack()
-
-win.mainloop()'''
-import tkinter
- 
-window_main = tkinter.Tk(className='Tkinter - TutorialKart', )
-window_main.geometry("400x200")
- 
-def submitFunction() :
-    print('Submit button is clicked.')
- 
-button_submit = tkinter.Button(window_main, text ="Submit", command=kek())
-button_submit.config(width=20, height=2)
- 
-button_submit.pack()
-window_main.mainloop()
+root.mainloop()
